d293597d8170ade96309a59d0a73674f48344a53
SOHTA0318 <nicosohta0318@gmail.com>
2022-03-01 02:45:28 -0800

    organized

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Layout from "./components/layout/Layout";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Form } from "react-bootstrap";
 import Selection from "./components/qrCode/Selection";
 
 function App() {
